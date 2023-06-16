297f04d43f1894086c887c99704e90d7c607ea66
Ivaylo <www.ivailo135@gmail.com>
2023-06-16 13:50:07 +0300

    Using the state hook

--- demo2/App.jsx
+++ demo2/App.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Button,
-  Linking,
 } from 'react-native';
 
 const App = () => {
+  // const [name, setName] = useState("Ivo");
+  // const [session, setSession] = useState({number: 6, title: "state"});
+  // const [current, setCurrent] = useState(true);
   
+  // const onClickHandler = () => {
+  //   setName("Programming with Ivo");
+  //   setSession({number: 7, title: "Style"});
+  //   setCurrent(false);
+  // }
+
+  const [count, setCount] = useState(0);
+  const [timesClicked, setTimesClicked] = useState(0);
+
+  const increment = () => {
+    setCount(count + 5);
+    setTimesClicked(timesClicked + 1);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming with Ivo</Text>
+
+      {/* <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
+      <Text style={styles.text}>{current ? "current session" : "next session"}</Text>
       <Button 
-        title="youtube channel"
-        onPress={() => {Linking.openURL("https://www.youtube.com/channel/UC6Ly_tnsu5sI5k4ZqGedelw")}}
-      ></Button>
+        title="Update State"
+        onPress={onClickHandler}
+      ></Button> */}
+      <Text style={styles.text}>{count}</Text>
+      <Button title="ADD" onPress={increment}></Button>
+      <Text style={styles.text}>You clicked {timesClicked} times</Text>
     </View>
   );
 }
